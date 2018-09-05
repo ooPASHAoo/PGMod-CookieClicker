0fa664d98a6718ebf56b4fa4a0e61d90581e2e04
Garskiy Pavel <Garskiy@wiseadvice.ru>
2018-09-05 21:16:06 +0300

    cursor price and buy funcs

--- PGMod-CooCli.js
+++ PGMod-CooCli.js
@@ -1,5 +1,27 @@
 // https://oopashaoo.github.io/PGMod-CookieClicker/PGMod-CooCli.js
 // Game.LoadMod('https://oopashaoo.github.io/PGMod-CookieClicker/PGMod-CooCli.js');
-console.log('start');
-product0.click()
-console.log('end');
+
+Products = {
+
+	prod0: {
+		descr: 'Cursor',
+		logPrice: () => { 
+			console.log(this.descr, ' price: ', productPrice0.value); 
+		},
+		buy: () => { 
+			console.log(this.descr, ' but done!'); 
+			product0.click(); 
+		},
+	}
+	
+}
+
+
+
+
+console.log('--- start ---');
+
+Products.prod0.logPrice();
+Products.prod0.buy();
+
+console.log('---- end ----');
